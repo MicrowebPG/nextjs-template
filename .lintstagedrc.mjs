7b348b556a9b8215e1139729477810f7d2d8fd08
Francesco Pauselli <81373986+francescopausellii@users.noreply.github.com>
2025-10-14 23:34:34 +0200

    fix(lint)!: fixed lint-staged not running correctly (#5)
    BREAKING CHANGE: fixed lint-staged not running correctly
    
    Husky pre-commit fails with Next.js 15.5 lint changes #2

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,10 +1,10 @@
 import path from 'path';
 
 const buildEslintCommand = (filenames) =>
-  `eslint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+  `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
 
 const config = {
-  '*.{ts,tsx}': () => 'npm run tsc',
+  '*.{ts,tsx}': () => 'tsc --noEmit',
   '*.{js,jsx,ts,tsx,json,md,css,scss,prettierrc}': 'prettier --write',
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 };
