b0a4129d8ef9ae93be68cc35ea9bc48f61732ea1
cecco25 <cecchinimichael.code@gmail.com>
2025-11-24 21:44:29 +0100

    fix(prisma): add mariadb driver

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,23 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../../generated/prisma/client';
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT ?? '3306'),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
