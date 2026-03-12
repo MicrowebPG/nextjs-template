88830c915e034f311b3d2d76ad632edce38b16fe
cecco25 <cecchinimichael.code@gmail.com>
2026-03-12 17:54:56 +0100

    chore: update linting tools to oxlint

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,12 +1,7 @@
-import path from 'path';
-
-const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
-
 const config = {
   '*.{ts,tsx}': () => 'tsc --noEmit',
   '*.{js,jsx,ts,tsx,json,md,css,scss,prettierrc}': 'prettier --write',
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': ['oxlint --fix'],
 };
 
 export default config;
