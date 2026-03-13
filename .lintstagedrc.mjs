7da97d9cf1d52936ac492e6fe6b5e7aca7a0b1fc
cecco25 <cecchinimichael.code@gmail.com>
2026-03-13 10:11:52 +0100

    chore: update lintstagedrc to include oxfmt and create fmt script

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,7 @@
 const config = {
   '*.{ts,tsx}': () => 'tsc --noEmit',
   '*.{js,jsx,ts,tsx,json,md,css,scss,prettierrc}': 'oxfmt --no-error-on-unmatched-pattern',
-  '*.{js,jsx,ts,tsx}': ['oxlint --fix']
+  '*.{js,jsx,ts,tsx}': ['oxlint --fix', 'oxfmt --no-error-on-unmatched-pattern']
 };
 
 export default config;
